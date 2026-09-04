59580f8e864e31ed43e61c8b14445e99f3717b33
Smrity Kumari <smritykumari7519@gmail.com>
2026-09-04 16:31:23 +0530

    Time: 6 ms (47.44%), Space: 121.5 MB (68.47%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,12 +1,31 @@
 class Solution {
-    public boolean uniformArray(int[] A) {
-        int min = A[0], odd = 0;
+    public boolean uniformArray(int[] nums1) {
 
-        for (int x : A) {
-            min = Math.min(min, x);
-            odd |= x & 1;
+        boolean hasOdd = false;
+        boolean hasEven = false;
+
+        for (int num : nums1) {
+            if (num % 2 == 0)
+                hasEven = true;
+            else
+                hasOdd = true;
         }
 
-        return (min & 1) == odd;
+        // Agar sirf ek hi parity hai
+        if (!hasOdd || !hasEven)
+            return true;
+
+        // Dono parity present hain
+        // Odd number ko even banane ke liye difference chahiye
+        // aur even number ko odd banane ke liye bhi.
+        // Possible only when minimum element is odd.
+        
+        int min = nums1[0];
+
+        for (int num : nums1) {
+            min = Math.min(min, num);
+        }
+
+        return min % 2 == 1;
     }
 }
